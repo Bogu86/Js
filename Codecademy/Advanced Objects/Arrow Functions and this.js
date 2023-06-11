754ede1c8b022bc5533dcc8c08f06360c3b859ec
Bogu86 <bogdan.lagodowicz@gmail.com>
2023-06-11 17:56:29 +0200

    this notation and arrow function

--- Codecademy/Advanced Objects/Arrow Functions and this.js
+++ Codecademy/Advanced Objects/Arrow Functions and this.js
@@ -1,1 +1,9 @@
-console.log("bez komitu");
+// avoid using arrow functions and this. notation
+const robot = {
+    energyLevel: 100,
+    checkEnergy()  {
+      console.log(`Energy is currently at ${this.energyLevel}%.`)
+    }
+  }
+  
+  robot.checkEnergy();
