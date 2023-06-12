d8a258076c751ae3d6622b123d66356920b637fa
Bogu86 <bogdan.lagodowicz@gmail.com>
2023-06-12 21:29:47 +0200

    go

--- Codecademy/Advanced Objects/Property Value Shorthand.js
+++ Codecademy/Advanced Objects/Property Value Shorthand.js
@@ -1,1 +1,15 @@
 //PROPERTY VALUE SHORTHAND ES6 new way of assiging properties to variables - known as destructuring.
+const robotFactory = (model, mobile) => {
+    return {
+      model,        // previous model : model,
+      mobile,        // same here
+      beep() {
+        console.log('Beep Boop');
+      }
+    }
+  }
+  
+  // To check that the property value shorthand technique worked:
+  const newRobot = robotFactory('P-501', false)
+  console.log(newRobot.model)
+  console.log(newRobot.mobile)
