e1eb881a60a0b487492fdf8e1e82d622c4c9f1c2
Bogu86 <bogdan.lagodowicz@gmail.com>
2023-06-12 20:00:54 +0200

    go

--- Codecademy/Advanced Objects/Factory Functions.js
+++ Codecademy/Advanced Objects/Factory Functions.js
@@ -1,4 +1,4 @@
-//FACTORY FUNCTIONS Let's us create many instances of an object quickly.
+//FACTORY FUNCTIONS Let's us create many instances of an object quickly. It returns an object and can be reused to make multiple object instances.
 const robotFactory = (model, mobile) => {
     return {
       model: model,
