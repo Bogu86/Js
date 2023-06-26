07dbd46bb18040ea6068ab25fd51bd908501427f
Bogu86 <bogdan.lagodowicz@gmail.com>
2023-06-26 21:47:44 +0200

    go

--- Codecademy/REACT/INTRO TO JSX/app2.js/src/App.js
+++ Codecademy/REACT/INTRO TO JSX/app2.js/src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-const myList = (
-  <ul>
-    <li>Jeden</li>
-    <li>Dwa</li>
-    <li>Tri</li>
-  </ul>
-);
+
 
 function App() {
   return (
     
     <div className="App">
       <header className="App-header">
-      {myList}
-      Kurwele
+      
+
+
+
+
       </header>
       
     </div>
