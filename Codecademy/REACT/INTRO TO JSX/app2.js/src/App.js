dba5b06d23bbfb8d3aab95db56ce31f9540effc0
Bogu86 <bogdan.lagodowicz@gmail.com>
2023-06-28 20:34:50 +0200

    REACT

--- Codecademy/REACT/INTRO TO JSX/app2.js/src/App.js
+++ Codecademy/REACT/INTRO TO JSX/app2.js/src/App.js
@@ -1,21 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import HelloWorld from './HelloWorld';
 
 
 function App() {
   return (
     
-    <div className="App">
-      <header className="App-header">
-      
-    Holy
-
-
-
-      </header>
-      
-    </div>
+    <HelloWorld/>
   );
 }
 
