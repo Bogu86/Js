8020457f7fc72c73155f1ed5f745b4295ab0e8de
Bogu86 <bogdan.lagodowicz@gmail.com>
2023-07-02 17:08:50 +0200

    gogogo

--- Training Ground Projects/REACT shenaningans/react-shenanigans/src/HighScore.js
+++ Training Ground Projects/REACT shenaningans/react-shenanigans/src/HighScore.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import styles from './HighScore.module.css';
 
 
 function HighScore(props) {
   return(
     <>
-    <h1>{props.commentObject.username}</h1>
-    <h2>{props.commentObject.date}</h2>
-    <h2>{props.commentObject.score}</h2>
+    
+      <div className={styles.ScoreBoard}>    
+    
+      <h1>{props.commentObject.username}</h1>
+      <h2>{props.commentObject.date}</h2>
+      <h2>{props.commentObject.score}</h2>
+    
+      </div>
+    
+
     </>
   );
   
